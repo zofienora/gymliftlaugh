92c754e34412e26facede9f4fcc760a4cb8bd973
Zofia Nora <sophienora92@gmail.com>
2024-12-07 14:03:04 +0100

    created structure and added the header and worked on main

---
+++ script.js
@@ -0,0 +1,11 @@
+// THE HAMBURGER 
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.getElementById("hamburger");
+    const navLinks = document.getElementById("nav-links");
+
+    hamburger.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
+    });
+});
+
+
